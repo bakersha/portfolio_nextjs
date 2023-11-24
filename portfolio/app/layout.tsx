bc8749e3310e00102dff3e7cdca3841007ab5d21
bakersha <sharayakt@gmail.com>
2023-11-23 23:07:28 -0800

    Actually fix the favicon

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Sharaya Baker',
   description: 'Full Stack Software Developer',
+  icons: {
+    icon: '/icon.ico'
+  }
 }
 
 export default function RootLayout({
